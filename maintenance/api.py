8d8183f101e3fdb54b915e4e3054bc79ca7b31c7
Thomas Jensen <32744171+Mast3rwaf1z@users.noreply.github.com>
2025-04-21 00:54:30 +0200

    feat/black-formatter (#24)
    * yoink and add black formatter
    
    * update black
    
    * formatting fix

--- maintenance/api.py
+++ maintenance/api.py
@@ -7,16 +7,18 @@ from subprocess import Popen, PIPE
 from fcntl import F_GETFL, F_SETFL, fcntl
 from threading import Lock
 
-script_dir:str = path.dirname(argv[0])
-maintenance_file:str = environ["MAINTENANCE_FILE"]
+script_dir: str = path.dirname(argv[0])
+maintenance_file: str = environ["MAINTENANCE_FILE"]
 
 processes: list[Popen] = []
-output_log_lock: Lock = Lock() 
+output_log_lock: Lock = Lock()
 output_log: list[str] = []
+
 
 def run_process(command: str):
     global processes
     processes.append(Popen(command, shell=True, stderr=PIPE))
+
 
 def read(output: IO) -> str:
     fd: int = output.fileno()
@@ -27,6 +29,7 @@ def read(output: IO) -> str:
     if text is None:
         return ""
     return text.decode()
+
 
 def read_process() -> str:
     toBeRemoved: list[Popen] = []
@@ -39,7 +42,7 @@ def read_process() -> str:
                 output += read(process.stderr)
             if process.stdout is not None:
                 output += read(process.stdout)
-    if output_log_lock.acquire(blocking=False): # Don't acquire if its locked
+    if output_log_lock.acquire(blocking=False):  # Don't acquire if its locked
         output_log_lock.locked_lock()
         output += "\n".join(output_log)
         output_log.clear()
@@ -49,15 +52,12 @@ def read_process() -> str:
         processes.remove(process)
     return output
 
+
 class Settings:
     def __init__(self) -> None:
         data: dict[str, Any]
         if not path.exists(maintenance_file):
-            data = {
-                "room_id":1,
-                "extra_packages":[],
-                "should_restart":False
-            }
+            data = {"room_id": 1, "extra_packages": [], "should_restart": False}
         else:
             with open(maintenance_file, "r") as file:
                 data = load(file)
@@ -65,48 +65,56 @@ class Settings:
         self.extra_packages: list[str] = data["extra_packages"]
         self.should_restart: bool = data["should_restart"]
 
-    def save(self, fp = None):
+    def save(self, fp=None):
         if fp is None:
             with open(maintenance_file, "w") as file:
-                file.write(dumps({
-                    "room_id":self.room_id,
-                    "extra_packages":self.extra_packages,
-                    "should_restart":self.should_restart
-                }))
+                file.write(
+                    dumps(
+                        {
+                            "room_id": self.room_id,
+                            "extra_packages": self.extra_packages,
+                            "should_restart": self.should_restart,
+                        }
+                    )
+                )
         else:
-            fp.write(dumps({
-                "room_id":self.room_id,
-                "extra_packages":self.extra_packages,
-                "should_restart":self.should_restart
-            }).encode())
+            fp.write(
+                dumps(
+                    {
+                        "room_id": self.room_id,
+                        "extra_packages": self.extra_packages,
+                        "should_restart": self.should_restart,
+                    }
+                ).encode()
+            )
+
 
 settings: Settings = Settings()
+
 
 def rebuild():
     run_process(f"{script_dir}/rebuild.sh")
 
+
 def restart():
     run_process("reboot")
 
+
 def switch_to_terminal():
-    kill_list: list[str] = [
-        "firefox",
-        "qsudo",
-        "alacritty"
-    ]
+    kill_list: list[str] = ["firefox", "qsudo", "alacritty"]
     for kill_target in kill_list:
         system(f"pkill -15 {kill_target}")
     system("alacritty")
 
+
 class Handler(BaseHTTPRequestHandler):
     def get_data(self) -> dict[str, Any]:
         data_size: int = int(self.headers.get("Content-Length", 0))
-        data:str = self.rfile.read(data_size).decode()
+        data: str = self.rfile.read(data_size).decode()
         if not data == "":
             return loads(data)
         else:
             return {}
-
 
     def do_GET(self) -> None:
         match self.path:
