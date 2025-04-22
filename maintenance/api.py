393d40cc8610bd10fba3b36519586249931a7834
Thomas Jensen <mast3r@skade.dev>
2025-04-23 00:42:15 +0200

    logging

--- maintenance/api.py
+++ maintenance/api.py
@@ -9,7 +9,7 @@ from subprocess import check_output
 
 script_dir: str = path.dirname(argv[0])
 
-services: list[str] = ["rebuild.service", "update.service", "refresh-settings.service", "refresh-inputs.service", "pull.service", "terminal.service"]
+services: list[str] = ["rebuild.service", "update.service", "refresh-settings.service", "refresh-inputs.service", "pull.service", "terminal.service", "nix-gc.service"]
 last_read: datetime = datetime.now()
 output_log_lock: Lock = Lock()
 output_log: list[str] = []
